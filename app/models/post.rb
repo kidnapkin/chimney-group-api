ec288057dac76fcdd2bcba790f8c43cc6676301b
kidnapkin <e.domotenko@gmail.com>
2016-11-30 11:04:38 +0000

    add validations / change media_type / change Gemfile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
     mount_uploader :thumbnail, ThumbnailUploader
     mount_uploader :media, MediaUploader
+    
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :thumbnail, presence: true
+    validates :media, presence: true
+    validates :media_type, presence: true
+    
+    enum media_type: [ :video, :audio ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129160522) do
+ActiveRecord::Schema.define(version: 20161130082143) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.string   "thumbnail"
     t.string   "media"
-    t.string   "media_type"
+    t.integer  "media_type"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
